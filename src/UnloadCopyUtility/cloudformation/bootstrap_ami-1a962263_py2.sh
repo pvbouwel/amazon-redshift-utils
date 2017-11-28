83b4f57dd4012479f315ca58910528cbe95e2c8e
Peter Van Bouwel <pbbouwel@amazon.com>
2017-11-28 21:21:18 +0000

    Add logic to await source cluster to be fully restored. Add policy that allows Redshift api access to both Source & Target cluster

--- src/UnloadCopyUtility/cloudformation/bootstrap_ami-1a962263_py2.sh
+++ src/UnloadCopyUtility/cloudformation/bootstrap_ami-1a962263_py2.sh
@@ -26,22 +26,6 @@ function stop_step() {
   fi
 }
 
-
-STEP_LABEL="Get IAM_INFO.json"
-start_step
-curl http://169.254.169.254/latest/meta-data/iam/info > IAM_INFO.json 2>>$STDERROR
-echo "Result=`cat IAM_INFO.json`" >>$STDOUT
-cat IAM_INFO.json | grep Success &>>/dev/null
-r=$? && stop_step $r
-
-STEP_LABEL="Get STACK details"
-STACK_NAME=`cat IAM_INFO.json | grep InstanceProfileArn | awk -F/ '{ print $2}'`
-REGION_NAME=`curl http://169.254.169.254/latest/meta-data/hostname | awk -F. '{print $2}'`
-STEP_LABEL="Get Cloudfromation Stack name (aws cloudformation describe-stacks --region ${REGION_NAME} --stack-name ${STACK_NAME})"
-aws cloudformation describe-stacks --region ${REGION_NAME} --stack-name ${STACK_NAME} >STACK_DETAILS.json 2>>$STDERROR
-r=$? && stop_step $r
-
-
 STEP_LABEL="Install Python pip (easy_install pip)"
 start_step
 easy_install pip >>$STDOUTPUT 2>>$STDERROR
@@ -56,3 +40,65 @@ STEP_LABEL="Install PyGreSQL using pip (pip install PyGreSQL)"
 start_step
 pip install PyGreSQL  >>$STDOUTPUT 2>>$STDERROR
 r=$? && stop_step $r
+
+STEP_LABEL="Get IAM_INFO.json"
+start_step
+curl http://169.254.169.254/latest/meta-data/iam/info > IAM_INFO.json 2>>$STDERROR
+echo "Result=`cat IAM_INFO.json`" >>$STDOUT
+cat IAM_INFO.json | grep Success &>>/dev/null
+r=$? && stop_step $r
+
+STEP_LABEL="Await full stack bootstrap"
+start_step
+STACK_NAME=`cat IAM_INFO.json | grep InstanceProfileArn | awk -F/ '{ print $2}'`
+max_minutes_to_wait=15
+minutes_waited=0
+while [ 1 = 1 ]
+do
+    if [ ${minutes_waited} = ${max_minutes_to_wait} ]
+    then
+        stop_step 100
+        break
+    else
+        aws cloudformation describe-stacks --region ${REGION_NAME} --stack-name ${STACK_NAME} | grep StackStatus | grep CREATE_COMPLETE &>/dev/null
+        if [ "$?" = "0" ]
+        then
+            stop_step 0
+            break;
+        else
+            echo "`date` Stack not ready yet" >> ${STDOUTPUT}
+            sleep 60
+        fi
+    fi
+done
+
+STEP_LABEL="Get STACK details"
+start_step
+REGION_NAME=`curl http://169.254.169.254/latest/meta-data/hostname | awk -F. '{print $2}'`
+STEP_LABEL="Get Cloudformation Stack name (aws cloudformation describe-stacks --region ${REGION_NAME} --stack-name ${STACK_NAME})"
+aws cloudformation describe-stacks --region ${REGION_NAME} --stack-name ${STACK_NAME} >STACK_DETAILS.json 2>>$STDERROR
+r=$? && stop_step $r
+
+SOURCE_CLUSTER_NAME=`grep -A 1 SourceClusterName STACK_DETAILS.json | grep OutputValue | awk -F\" '{ print $4}'`
+STEP_LABEL="Await Redshift restore of source cluster (${SOURCE_CLUSTER_NAME})"
+start_step
+max_minutes_to_wait=20
+minutes_waited=0
+while [ 1 = 1 ]
+do
+    if [ ${minutes_waited} = ${max_minutes_to_wait} ]
+    then
+        stop_step 100
+        break
+    else
+        aws redshift describe-clusters --cluster-identifier ${SOURCE_CLUSTER_NAME} --region ${REGION_NAME} | grep -A 5  RestoreStatus | grep "\"Status\"" | grep completed
+        if [ "$?" = "0" ]
+        then
+            stop_step 0
+            break;
+        else
+            echo "`date` Stack not ready yet" >> ${STDOUTPUT}
+            sleep 60
+        fi
+    fi
+done
