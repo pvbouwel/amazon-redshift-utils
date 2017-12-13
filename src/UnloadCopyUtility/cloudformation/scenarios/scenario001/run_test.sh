f672b0ab104da2dd0f9a786221f8245092285217
Peter Van Bouwel <pbbouwel@amazon.com>
2017-12-13 20:30:27 +0000

    Improve logging in test scenarios

--- src/UnloadCopyUtility/cloudformation/scenarios/scenario001/run_test.sh
+++ src/UnloadCopyUtility/cloudformation/scenarios/scenario001/run_test.sh
@@ -17,7 +17,7 @@ cat >${HOME}/scenario001.json <<EOF
   },
   "s3Staging": {
     "aws_iam_role": "${S3CopyRole}",
-    "path": "s3://${CopyUnloadBucket}/scenario1/",
+    "path": "s3://${CopyUnloadBucket}/scenario001/",
     "deleteOnSuccess": "True",
     "region": "eu-west-1",
     "kmsGeneratedKey": "True"
@@ -39,26 +39,26 @@ r=$? && stop_step $r
 
 start_step "Generate DDL for table public.dwdate on target cluster"
 #Extract DDL
-psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select ddl from admin.v_generate_tbl_ddl where schemaname='ssb' and tablename='dwdate';" | awk '/CREATE TABLE/{flag=1}/ ;$/{flag=0}flag' | sed 's/ssb/public/' >${HOME}/scenario001.ddl.sql
+psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select ddl from admin.v_generate_tbl_ddl where schemaname='ssb' and tablename='dwdate';" | awk '/CREATE TABLE/{flag=1}/ ;$/{flag=0}flag' | sed 's/ssb/public/' >${HOME}/scenario001.ddl.sql 2>>${STDERROR}
 increment_step_result $?
 cat ${HOME}/scenario001.ddl.sql >>${STDOUTPUT} 2>>${STDERROR}
 increment_step_result $?
 stop_step ${STEP_RESULT}
 
 start_step "Drop table public.dwdate in target cluster if it exists"
-psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -c "DROP TABLE IF EXISTS public.dwdate;" 2>>${STDERROR} | grep "DROP TABLE"
+psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -c "DROP TABLE IF EXISTS public.dwdate;" 2>>${STDERROR} | grep "DROP TABLE"  >>${STDOUTPUT} 2>>${STDERROR}
 r=$? && stop_step $r
 
 
 start_step "Create table public.dwdate in target cluster"
-psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -f ${HOME}/scenario001.ddl.sql | grep "CREATE TABLE"
+psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -f ${HOME}/scenario001.ddl.sql | grep "CREATE TABLE"  >>${STDOUTPUT} 2>>${STDERROR}
 r=$? && stop_step $r
 
 
 start_step "Run Unload Copy Utility"
 source ${VIRTUAL_ENV_PY36_DIR}/bin/activate >>${STDOUTPUT} 2>>${STDERROR}
 cd ${HOME}/amazon-redshift-utils/src/UnloadCopyUtility && python3 redshift_unload_copy.py ${HOME}/scenario001.json eu-west-1 >>${STDOUTPUT} 2>>${STDERROR}
-EXPECTED_COUNT=`psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select 'count='||count(*) from ssb.dwdate;" | grep "count=[0-9]*"|awk -F= '{ print $2}'`
+EXPECTED_COUNT=`psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select 'count='||count(*) from ssb.dwdate;" | grep "count=[0-9]*"|awk -F= '{ print $2}'`  >>${STDOUTPUT} 2>>${STDERROR}
 psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -c "select 'count='||count(*) from public.dwdate;" | grep "count=${EXPECTED_COUNT}" >>${STDOUTPUT} 2>>${STDERROR}
 r=$? && stop_step $r
 deactivate
--- src/UnloadCopyUtility/cloudformation/scenarios/scenario003/run_test.sh
+++ src/UnloadCopyUtility/cloudformation/scenarios/scenario003/run_test.sh
@@ -22,7 +22,7 @@ cat >${HOME}/scenario003.json <<EOF
   },
   "s3Staging": {
     "aws_iam_role": "${S3CopyRole}",
-    "path": "s3://${CopyUnloadBucket}/scenario1/",
+    "path": "s3://${CopyUnloadBucket}/scenario003/",
     "deleteOnSuccess": "True",
     "region": "eu-west-1",
     "kmsGeneratedKey": "False"
@@ -43,25 +43,25 @@ r=$? && stop_step $r
 
 start_step "Generate DDL for table public.dwdate on target cluster"
 #Extract DDL
-psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select ddl from admin.v_generate_tbl_ddl where schemaname='ssb' and tablename='dwdate';" | awk '/CREATE TABLE/{flag=1}/ ;$/{flag=0}flag' | sed 's/ssb/public/' >${HOME}/scenario003.ddl.sql
+psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select ddl from admin.v_generate_tbl_ddl where schemaname='ssb' and tablename='dwdate';" | awk '/CREATE TABLE/{flag=1}/ ;$/{flag=0}flag' | sed 's/ssb/public/' >${HOME}/scenario003.ddl.sql 2>>${STDERROR}
 increment_step_result $?
 cat ${HOME}/scenario003.ddl.sql >>${STDOUTPUT} 2>>${STDERROR}
 increment_step_result $?
 stop_step ${STEP_RESULT}
 
 start_step "Drop table public.dwdate in target cluster if it exists"
-psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -c "DROP TABLE IF EXISTS public.dwdate;" 2>>${STDERROR} | grep "DROP TABLE"
+psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -c "DROP TABLE IF EXISTS public.dwdate;" 2>>${STDERROR} | grep "DROP TABLE" >>${STDOUTPUT} 2>>${STDERROR}
 r=$? && stop_step $r
 
 start_step "Create table public.dwdate in target cluster"
-psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -f ${HOME}/scenario003.ddl.sql | grep "CREATE TABLE"
+psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -f ${HOME}/scenario003.ddl.sql | grep "CREATE TABLE" >>${STDOUTPUT} 2>>${STDERROR}
 r=$? && stop_step $r
 
 
 start_step "Run Unload Copy Utility"
 source ${VIRTUAL_ENV_PY27_DIR}/bin/activate >>${STDOUTPUT} 2>>${STDERROR}
 cd ${HOME}/amazon-redshift-utils/src/UnloadCopyUtility && python2 redshift_unload_copy.py ${HOME}/scenario003.json eu-west-1 >>${STDOUTPUT} 2>>${STDERROR}
-EXPECTED_COUNT=`psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select 'count='||count(*) from ssb.dwdate;" | grep "count=[0-9]*"|awk -F= '{ print $2}'`
+EXPECTED_COUNT=`psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select 'count='||count(*) from ssb.dwdate;" | grep "count=[0-9]*"|awk -F= '{ print $2}'` >>${STDOUTPUT} 2>>${STDERROR}
 psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -c "select 'count='||count(*) from public.dwdate;" | grep "count=${EXPECTED_COUNT}" >>${STDOUTPUT} 2>>${STDERROR}
 r=$? && stop_step $r
 deactivate
