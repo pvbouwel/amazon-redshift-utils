bcd45e577aeac8139428767a3c117959def61542
Peter Van Bouwel <pbbouwel@amazon.com>
2018-01-27 17:26:57 +0000

    Improve documentation on password in config file to advertise the automatic password retrieval using getClusterCredentials. Change scenario001 to be the documented scenario. Remove _py2 suffix from bootstrap script since it bootstraps both a Python2 and a Python3 environment. Make sure template matches the new bootstrap filename. Improve documentation in global_config_parameters.json Change overall documentation to advertise new functionality and to make sure dependencies are documented.

--- src/UnloadCopyUtility/tests/cloudformation/scenarios/scenario001/run_test.sh
+++ src/UnloadCopyUtility/tests/cloudformation/scenarios/scenario001/run_test.sh
@@ -10,6 +10,7 @@ TARGET_TABLE="${SOURCE_TABLE}"
 PYTHON="python3"
 
 DESCRIPTION="Perform Unload Copy with password encrypted using KMS, expect target location to be correct. "
+DESCRIPTION="${DESCRIPTION}Use invocation as per documentation  --s3-config-file s3://${CopyUnloadBucket}/my-unload-copy-config.json --region eu-west-1"
 DESCRIPTION="${DESCRIPTION}Use ${PYTHON}."
 
 start_scenario "${DESCRIPTION}"
@@ -49,6 +50,11 @@ EOF
 cat ${HOME}/${SCENARIO}.json >>${STDOUTPUT} 2>>${STDERROR}
 r=$? && stop_step $r
 
+start_step "Push config file to S3 "
+aws s3 cp "${HOME}/${SCENARIO}.json" "s3://${CopyUnloadBucket}/my-unload-copy-config.json" >>${STDOUTPUT} 2>>${STDERROR}
+r=$? && stop_step $r
+
+
 start_step "Generate DDL for table ${SOURCE_SCHEMA}.${SOURCE_TABLE} on target cluster"
 #Extract DDL
 psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpointPort} -U ${SourceClusterMasterUsername} ${SourceClusterDBName} -c "select ddl from admin.v_generate_tbl_ddl where schemaname='${SOURCE_SCHEMA}' and tablename='${SOURCE_TABLE}';" | awk '/CREATE TABLE/{flag=1}/ ;$/{flag=0}flag' | sed "s/${SOURCE_SCHEMA}/${TARGET_SCHEMA}/" >${HOME}/${SCENARIO}.ddl.sql 2>>${STDERROR}
@@ -73,5 +79,9 @@ EXPECTED_COUNT=`psql -h ${SourceClusterEndpointAddress} -p ${SourceClusterEndpoi
 psql -h ${TargetClusterEndpointAddress} -p ${TargetClusterEndpointPort} -U ${TargetClusterMasterUsername} ${TargetClusterDBName} -c "select 'count='||count(*) from ${TARGET_SCHEMA}.${TARGET_TABLE};" | grep "count=${EXPECTED_COUNT}" >>${STDOUTPUT} 2>>${STDERROR}
 r=$? && stop_step $r
 deactivate
+
+start_step "Remove config file from S3"
+aws s3 rm "s3://${CopyUnloadBucket}/my-unload-copy-config.json" >>${STDOUTPUT} 2>>${STDERROR}
+r=$? && stop_step $r
 
 stop_scenario
